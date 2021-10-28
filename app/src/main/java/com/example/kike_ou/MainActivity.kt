d0fc199d59a498f649f0b92d422d3731751155f7
C-Plantet <cplantet@enssat.fr>
2021-10-28 12:29:01 +0200

    QRGen

--- app/src/main/java/com/example/kike_ou/MainActivity.kt
+++ app/src/main/java/com/example/kike_ou/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.kike_ou
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val QrcodeGen = Intent(this,QRGeneratorActivity::class.java)
+
+        val boutonGenQR : Button = findViewById(R.id.buttonGenQR)
+
+        boutonGenQR.setOnClickListener{
+            startActivity(QrcodeGen)
+        }
     }
 }
