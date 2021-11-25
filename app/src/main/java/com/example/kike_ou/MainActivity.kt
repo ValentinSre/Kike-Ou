ed1c724448cdd3b2f370255f9682029f8be6e4bf
C-Plantet <cplantet@enssat.fr>
2021-11-25 08:44:21 +0100

    QRCode Scan qui marche pô

--- app/src/main/java/com/example/kike_ou/MainActivity.kt
+++ app/src/main/java/com/example/kike_ou/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.kike_ou.QRScanActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,11 +14,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val QrcodeGen = Intent(this,QRGeneratorActivity::class.java)
+        val QR_scan = Intent(this, QRScanActivity::class.java)
 
         val boutonGenQR : Button = findViewById(R.id.buttonGenQR)
+        val boutonCamera : Button = findViewById(R.id.camera_button)
+
 
         boutonGenQR.setOnClickListener{
             startActivity(QrcodeGen)
+        }
+
+        boutonCamera.setOnClickListener{
+            startActivity(QR_scan)
         }
     }
 }
